1c9622d4276e06580980c3035488662075ef1ea5
Kasey Linden <kaseylinden@gmail.com>
2020-12-03 14:56:29 -0600

    complete

--- controls/V-218756.rb
+++ controls/V-218756.rb
@@ -47,5 +47,26 @@ not otherwise hosting any content, this requirement is Not Applicable.
   tag fix_id: 'F-20227r311167_fix'
   tag cci: ['SV-109337', 'V-100233', 'CCI-001094']
   tag nist: ['SC-5 (1)']
+
+  get_names = command("Get-Website | select name | findstr /v 'name ---'").stdout.strip.split("\r\n")
+  get_allowHighBitCharacters = command('Get-WebConfigurationProperty -pspath "IIS:\Sites\*" -Filter system.webServer/security/requestFiltering -name * | select -expand allowHighBitCharacters').stdout.strip.split("\r\n")
+
+  get_allowHighBitCharacters.zip(get_names).each do |allowHighBitCharacters, names|
+    n = names.strip
+
+    describe "The IIS site: #{n} websites Allow high-bit characters" do
+      subject { allowHighBitCharacters }
+      it { should cmp 'True' }
+    end
+  end
+  if get_names.empty?
+    impact 0.0
+    desc 'There are no IIS sites configured hence the control is Not-Applicable'
+
+    describe 'No sites where found to be reviewed' do
+      skip 'No sites where found to be reviewed'
+    end
+  end
+
 end
 
